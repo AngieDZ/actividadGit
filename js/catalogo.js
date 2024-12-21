a29187cdba310ef11fa5b545161c40591fea696a
Bayron_Chavarria <bayronchava@gmail.com>
2024-12-21 00:25:57 -0500

    Agregar funcionalidad para mostrar y ocultar el carrito

---
+++ js/catalogo.js
@@ -0,0 +1,25 @@
+const btnCart = document.querySelector('.container-cart-icon');
+const containerCartProducts = document.querySelector(
+	'.container-cart-products'
+);
+
+btnCart.addEventListener('click', () => {
+	containerCartProducts.classList.toggle('hidden-cart');
+});
+
+
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+
+const productsList = document.querySelector('.container-items');
+
+
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
